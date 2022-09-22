bee4a56c6f0c7dbf53d694dc8f3fa621fe73be48
Eager-Blockchain-Dev <112506679+01blockchainDev@users.noreply.github.com>
2022-09-21 23:36:28 -0400

    Update IFOUpgradeProxy.sol
    first step

--- makiswap-ifo-master/makiswap-ifo-master/contracts/IFOUpgradeProxy.sol
+++ makiswap-ifo-master/makiswap-ifo-master/contracts/IFOUpgradeProxy.sol
@@ -1,11 +1,3 @@
-pragma solidity 0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-
-contract IFOUpgradeProxy is TransparentUpgradeableProxy {
-
-    constructor(address admin, address logic, bytes memory data) TransparentUpgradeableProxy(logic, admin, data) public {
-
-    }
-
-}
